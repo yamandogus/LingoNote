6e577ee8abfd43c567d878ac00d3ee8b2a01455d
yamandogus <yamandogus251@gmail.com>
2025-06-25 21:59:22 +0300

    Add note retrieval, update, and deletion functionalities in noteController

--- backend/src/controllers/noteController.js
+++ backend/src/controllers/noteController.js
@@ -20,7 +20,6 @@ export const createNote = async (req, res) => {
     res
       .status(201)
       .json({ message: "Not başarıyla oluşturuldu.", note: newNote });
-    res.status(201).json();
   } catch (error) {
     res.status(500).json({ message: "Sunucu hatası", error: error.message });
   }
@@ -33,6 +32,73 @@ export const getNotes = async (req, res) => {
     const userId = req.user.userId;
     const notes = await Note.find({ user: userId });
     res.status(200).json({ notes });
+  } catch (error) {
+    res.status(500).json({ message: "Sunucu hatası", error: error.message });
+  }
+};
+
+// Tek bir notu getirme
+
+export const getNoteById = async (req, res) => {
+  try {
+    const noteId = req.params.id;
+    const userId = req.user.userId;
+    const note = await Note.findOne({ _id: noteId, user: userId });
+    if (!note) {
+      return res.status(404).json({ message: "Not bulunamadı." });
+    }
+    res.status(200).json(note);
+  } catch (error) {
+    res.status(500).json({ message: "Sunucu hatası", error: error.message });
+  }
+};
+
+// Not güncelleme
+export const updateNote = async (req, res) => {
+  try {
+    const noteId = req.params.id;
+    const userId = req.user.userId;
+    const { title, content, category } = req.body;
+
+    const updatedNote = await Note.findOneAndUpdate(
+      {
+        _id: noteId,
+        user: userId,
+      },
+      {
+        title,
+        content,
+        category,
+      },
+      { new: true }
+    );
+
+    if (!updatedNote) {
+      return res
+        .status(404)
+        .json({ message: "Not bulunamadı veya güncellenemedi." });
+    }
+    res.status(200).json({ message: "Not güncellendi.", note: updatedNote });
+  } catch (error) {
+    res.status(500).json({ message: "Sunucu hatası", error: error.message });
+  }
+};
+
+// Not Silme
+
+export const deleteNote = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const noteId = req.params.id;
+    const deletedNote = await Note.findOneAndDelete({
+      _id: noteId,
+      user: userId,
+    });
+
+    if (!deletedNote) {
+      res.status(400).json({ message: "Not bulunamadı veya silinemedi." });
+    }
+    res.status(200).json({ message: "Not silindi" });
   } catch (error) {
     res.status(500).json({ message: "Sunucu hatası", error: error.message });
   }
