acf20c2435e6496e5118fd25783c8a8557171db1
yamandogus <yamandogus251@gmail.com>
2025-06-26 22:59:08 +0300

    Add MongoDB connection setup using Mongoose in db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,1 +1,21 @@
-// Bu dosyada PostgreSQL veritabanı bağlantısı yapılacak. 
+// Bu dosyada PostgreSQL veritabanı bağlantısı yapılacak.
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+     console.log("MongoDB bağlantısı başarılı!");
+  } catch (error) {
+    console.error("MongoDB bağlantı hatası:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
