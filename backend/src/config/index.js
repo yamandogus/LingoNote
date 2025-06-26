e798e71cb3e8a267fa3d66806118a6a7fe8ab62b
yamandogus <yamandogus251@gmail.com>
2025-06-26 23:11:06 +0300

    Add environment configuration and default settings for JWT and PORT in index.js

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,1 +1,6 @@
-// Diğer konfigürasyonlar burada yönetilecek. 
+import dotenv from "dotenv";
+dotenv.config();
+
+export const JWT_SECRET = process.env.JWT_SECRET || "gizli_anahtar";
+export const PORT = process.env.PORT || 3000;
+// Başka configler de ekleyebilirsin
