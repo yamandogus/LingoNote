826269ec61264ed466f4740bc7e508aca616a1f0
yamandogus <yamandogus251@gmail.com>
2025-06-26 22:43:45 +0300

    Add password hashing and comparison functions using bcrypt in hash.js

--- backend/src/utils/hash.js
+++ backend/src/utils/hash.js
@@ -1,1 +1,10 @@
-// Şifreleme (hash) işlemleri burada yapılacak. 
+// Şifreleme (hash) işlemleri burada yapılacak.
+import bcrypt from "bcrypt";
+
+export const hashPassword = async (password) => {
+  return await bcrypt.hash(password, 10);
+};
+
+export const comparePassword = async(password, hashPassword) =>{
+    return await bcrypt.compare(password, hashPassword);
+}
--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -1,1 +1,8 @@
-// Winston ile loglama işlemleri burada yapılacak. 
+// Winston ile loglama işlemleri burada yapılacak.
+import winston from "winston";
+
+const logger = winston.createLogger({
+  lavel: "info",
+  format: winston.format.json(),
+  transports: [new winston.transports.Console()],
+});
