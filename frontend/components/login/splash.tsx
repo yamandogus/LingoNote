bd53d895e6139e52f5de671ff23caed406ac706d
yamandogus <yamandogus251@gmail.com>
2025-07-06 11:45:58 +0300

    Add react-native-typed-text dependency and enhance splash screen layout

--- frontend/components/login/splash.tsx
+++ frontend/components/login/splash.tsx
@@ -28,15 +28,48 @@ export function SplashScreen({
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
     >
+      <View className="pt-16 flex-row justify-center space-x-1">
+        {[
+          { char: "L", y: 6 },
+          { char: "i", y: 4 },
+          { char: "n", y: -2 },
+          { char: "g", y: -5 },
+          { char: "o", y: -7 },
+          { char: "N", y: -4 },
+          { char: "o", y: -2 },
+          { char: "t", y: 0 },
+          { char: "e", y: 6 },
+        ].map(({ char, y }, index) => (
+          <Text
+            key={index}
+            className="uppercase"
+            style={{
+              transform: [{ translateY: y }],
+              fontSize: 50,
+              fontWeight: "900",
+              fontFamily: "sans-serif-condensed", // istersen custom fontla değiştir
+              color: index >= 5 ? "#3B82F6" : isDark ? "white" : "#111827",
+              textShadowColor: "rgba(0,0,0,0.3)",
+              textShadowOffset: { width: 1, height: 2 },
+              textShadowRadius: 4,
+            }}
+          >
+            {char}
+          </Text>
+        ))}
+      </View>
+
       <View className="flex-1 items-center justify-center space-y-4">
         <View className="w-28 h-28 border-8 border-gray-300 border-t-blue-400 rounded-full items-center justify-center animate-spin">
           <Svg viewBox="0 0 24 24" fill="blue" height={24} width={24}>
             <Path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z" />
           </Svg>
         </View>
-        <Text className="text-blue-500 text-lg font-semibold">
-          LingoNote Yükleniyor...
-        </Text>
+        <View className="mt-10">
+          <Text className="dark:text-gray-300 font-bold">
+            LingoNote Yükleniyor...
+          </Text>
+        </View>
       </View>
     </LinearGradient>
   );
